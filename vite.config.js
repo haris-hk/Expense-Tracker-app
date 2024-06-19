fcc665ea4d2b81cf5548d5e25a09f42bbe251cdb
haris-hk <66428634+haris-hk@users.noreply.github.com>
2024-06-19 13:20:41 +0500

    done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Expense-Tracker-app/" : "/",
+};
 export default defineConfig({
   base: '/Expense-Tracker-app/',
   plugins: [
